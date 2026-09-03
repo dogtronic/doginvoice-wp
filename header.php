1504edba6ef267cc167ecb42e50f4db99fde2ac1
Marek Golan <marek.golan@dogtronic.io>
2026-09-03 13:22:19 +0200

    Update header.php

--- header.php
+++ header.php
@@ -55,6 +55,9 @@
     }
   </script>
 
+<script>!function(w,d,s,u){if(w.oaiq)return;var q=function(){q.q.push(arguments)};q.q=[];w.oaiq=q;var j=d.createElement(s);j.async=1;j.src=u;var f=d.getElementsByTagName(s)[0];f.parentNode.insertBefore(j,f)}(window,document,"script","https://bzrcdn.openai.com/sdk/oaiq.min.js");oaiq("init",{pixelId:"HQvMsVp6MkdU7SU7g3EAmM",debug:true});</script>
+
+  
 </head>
 
 <body <?php body_class(); ?>>
